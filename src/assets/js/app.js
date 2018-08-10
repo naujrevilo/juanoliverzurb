0a5fa44a799a5924acbfde3fe07747ce7eeea9bd
nauj <joliver@juanoliver.net>
2018-08-09 22:12:11 -0500

    Inicial. Nuevo sitio.

---
+++ src/assets/js/app.js
@@ -0,0 +1,135 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+
+import Foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+import 'tablesaw/dist/tablesaw.jquery';
+import libs from './lib/dependancies';
+window.libs = libs;
+
+$(document).foundation();
+
+libs.AOS.init();
+
+// SVG Injector
+// Elements to inject
+var mySVGsToInject = document.querySelectorAll('img.inject-me');
+
+// Options
+var injectorOptions = {
+  evalScripts: 'once',
+  pngFallback: 'assets/png'
+};
+
+var afterAllInjectionsFinishedCallback = function (totalSVGsInjected) {
+  // Callback after all SVGs are injected
+  console.log('We injected ' + totalSVGsInjected + ' SVG(s)!');
+};
+
+var perInjectionCallback = function (svg) {
+  // Callback after each SVG is injected
+  console.log('SVG injected: ' + svg);
+};
+
+// create injector configured by options
+var injector = new libs.svgInjector(injectorOptions);
+
+// Trigger the injection
+injector.inject(
+  mySVGsToInject,
+  afterAllInjectionsFinishedCallback,
+  perInjectionCallback
+);
+
+// slick carousel
+$(".content-carousel").slick({
+  // normal options...
+  speed: 5000,
+	autoplay: true,
+	autoplaySpeed: 0,
+	cssEase: 'linear',
+  slidesToShow: 5,
+	slidesToScroll: 1,
+  infinite: true,
+  swipeToSlide: true,
+	centerMode: true,
+  focusOnSelect: true,
+  // the magic
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+    }, {
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+    }]
+});
+
+// tablesaw table plugin
+$(function () {
+  $(document)
+    .foundation()
+    .trigger('enhance.tablesaw');
+});
+
+var TablesawConfig = {
+  swipeHorizontalThreshold: 15
+};
+
+// app dashboard toggle
+$('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
+  e.preventDefault();
+  $(this).parents('.app-dashboard').toggleClass('shrink-medium').toggleClass('shrink-large');
+});
+
+$(function() {
+    $(window).scroll(function() {
+        var winTop = $(window).scrollTop();
+        if (winTop >= 30) {
+            $("body").addClass("sticky-shrinknav-wrapper");
+        } else{
+            $("body").removeClass("sticky-shrinknav-wrapper");
+        }
+    });
+});
+
+// Salesiq Zoho
+var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq ||
+	{widgetcode:"e5deb93e27db9943910425f8b80bfb80f1a198f8d7d1b640bef67b696b281b0b", values:{},ready:function(){}};
+var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;
+s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
+
+//Global site tag (gtag.js) - Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-26191186-1');
+
+
+//Comentarios Disqus
+
+var disqus_config = function () {
+    this.page.url = '/blog/deteccion_de_intrusos_redes_WiFi.html';  // Replace PAGE_URL with your page's canonical URL variable
+    //this.page.identifier = '1'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://juanolivernet.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
